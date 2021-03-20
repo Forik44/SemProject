3a9015cb7b631c15780b2a14de295c71d3bdf7ad
Ermolaefff <fallen2320@yandex.ru>
2021-03-20 12:46:16 +0300

    Added a marker in Dict.h

--- SemProject/Dict.h
+++ SemProject/Dict.h
@@ -2,21 +2,93 @@
 #include "Array.h"
 template<typename  Chemu, typename Chto> class UniDict
 {
+private:
+    struct Para {
+        Chemu che;
+        Chto cht;
+    };
+    Array<Para> m_storage;
+    struct marker
+    {
+        Para* Elem;
+        size_t Idx;
+        size_t markerSize;
+    };
+    size_t size;
+    friend class Marker;
 public:
 	Dict();
-	class Marker {
+    void add(Chemu che, Chto cht);
+    Chto& operator[](Chemu che);
 
-	};
-	void add(Chemu che, Chto cht);
-	Marker find(Chemu);
-	Chto& operator[](Chemu che);
+    class Marker
+    {
+        marker mark;
+    public:
+        friend class UniDict;
+        Para& operator*()
+        {
+            return *mark.Elem;
+        };
+        const Para& operator*() const
+        {
+            return *mark.Elem;
+        };
+        void operator++()
+        {
+            mark.Elem++;
+            mark.Idx++;
+        };
+        void operator++(int)
+        {
+            mark.Elem++;
+            mark.Idx++;
+        };
+        bool operator==(const Marker& secondMarker) const
+        {
+            return (this->mark.Elem == secondMarker.mark.Elem && this->mark.Idx == secondMarker.mark.Idx);
+        };
+        bool operator!=(const Marker& secondMarker) const
+        {
+            return !(*this == secondMarker);
+        }
+        bool canMoveNext()
+        {
+            return (mark.Idx < mark.markerSize);
+        };
+        void setMarkerSize(size_t size)
+        {
+            mark.markerSize = size;
+        }
 
-private:
-	struct Para {
-		Chemu che;
-		Chto cht;
-	};
-	Array<Para> m_storage;
+    };
+
+    Marker init()
+    {
+        Marker ma;
+        ma.setMarkerSize(size);
+        ma.mark.Elem = m_storage.init();
+        ma.mark.Idx = 0;
+        return ma;
+    };
+    Marker afterEnd()
+    {
+        Marker ma;
+        ma.setMarkerSize(size);
+        ma.mark.Elem = m_storage.init() + size;
+        ma.mark.Idx = ma.mark.markerSize;
+        return ma;
+    };
+    Marker initAfterAddingNewElement()
+    {
+        Marker ma;
+        ma.setMarkerSize(size);
+        ma.mark.Elem = m_storage.init() + size - 2;
+        ma.mark.Idx = size - 2;
+        return ma;
+    };
+
+    Marker find(Chemu);
 
 };
 
