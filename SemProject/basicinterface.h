be6fc26e1f844559413ab4cc01284d86fb1bf7d9
Ermolaefff <fallen2320@yandex.ru>
2021-03-20 11:24:05 +0300

    .

--- SemProject/basicinterface.h
+++ SemProject/basicinterface.h
@@ -3,27 +3,23 @@
 #include "objects.h"
 #include "Array.h"
 #include "List.h"
+#include "Dict.h"
 
-struct Parameter
+enum ParamType
 {
-    enum ParamType
-    {
-        PT_ERROR,
-        PT_POINT,
-        PT_CIRCLE,
-        PT_SEGMENT,
-        PT_CX,
-        PT_CY,
-        PT_R,
-        PT_P1X,
-        PT_P2X,
-        PT_P1Y,
-        PT_P2Y,
-        PT_PX,
-        PT_PY
-    };
-    ParamType type;
-    double value;
+    PT_ERROR,
+    PT_POINT,
+    PT_CIRCLE,
+    PT_SEGMENT,
+    PT_CX,
+    PT_CY,
+    PT_R,
+    PT_P1X,
+    PT_P2X,
+    PT_P1Y,
+    PT_P2Y,
+    PT_PX,
+    PT_PY
 };
 
 class BasicInterface
@@ -35,20 +31,27 @@ public:
     bool removeObject(ID);
   /*  bool changeObjectProperty(ID);*/
     ID  addRequirement(Array<ID>&, ReqType);
-    Array<Parameter> queryObjProperties(ID);
+    Dict<ParamType, double> queryObjProperties(ID);
     ObjType identifyObjTypeByID(ID);
     bool removeRequirement(ID);
 
 private:
-    Array<Identifiable<Point>> m_points;
+
+    Dict<ID, Point> m_points;
+    Dict<ID, Circle> m_circles;
+    Dict<ID, Segment> m_segments;
+    Dict<ID, Requirement> m_requirements;
+
+   /* Array<Identifiable<Point>> m_points;
     Array<Identifiable<Segment> > m_segments;
     Array<Identifiable<Circle> > m_circles;
 
     List<Identifiable<Point>> l_points;
     List<Identifiable<Segment>> l_segmentss;
     List<Identifiable<Circle>> l_circles;
-
+  
     Array<Identifiable<Requirement> > m_requirements;
+      */
 };
 
 
