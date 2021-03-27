2e3793d1c400666ce1d8b315c0a12a6db13e7b69
Ermolaefff <fallen2320@yandex.ru>
2021-03-27 11:01:38 +0300

    Added add method

--- SemProject/TreeDict.h
+++ SemProject/TreeDict.h
@@ -39,10 +39,10 @@ template<typename Chemu, typename Chto> Chto& TreeDict<Chemu, Chto>::operator[](
         if (cur->data.che == che)
             return cur->data.cht;
         else if (che < cur->data.che)
-            next = &cur->left; 
+            next = &cur->left;
         else if (che > cur->data.che)
             next = &cur->right;
-   
+
         if (*next == nullptr)
         {
             Node* tmp = new Node;
@@ -55,6 +55,36 @@ template<typename Chemu, typename Chto> Chto& TreeDict<Chemu, Chto>::operator[](
         }
 
         cur = *next;
-        
+
     }
+}
+
+template<typename Chemu, typename Chto> void TreeDict<Chemu, Chto>::add(Chemu che, Chto cht)
+{
+    Node* cur = m_root;
+    Node** next;
+    while (cur != nullptr)
+    {
+        if (cur->data.che == che)
+            return;
+        else if (che < cur->data.che)
+            next = &cur->left;
+        else if (che > cur->data.che)
+            next = &cur->right;
+
+        if (*next == nullptr)
+        {
+            Node* tmp = new Node;
+            tmp->left = tmp->right = nullptr;
+            Para p;
+            p.che = che;
+            p.cht = cht;
+            tmp->data = p;
+            *next = tmp;
+            return;
+        }
+
+        cur = *next;
+    }
+
 }
