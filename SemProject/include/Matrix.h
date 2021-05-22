2fbb375ee04fe599e44797754e7ea7ce5ef7c740
Ermolaefff <fallen2320@yandex.ru>
2021-05-22 18:42:30 +0300

    .

--- SemProject/include/Matrix.h
+++ SemProject/include/Matrix.h
@@ -6,7 +6,122 @@ template<typename MatrixElement> class Matrix {
 private:
     int _v_size, _h_size;
     MatrixElement** data;
+    void subLines(MatrixElement* minuendLine, MatrixElement* subsLine, int size)
+    {
+        for (int i = 0; i < size; i++)
+        {
+            minuendLine[i] -= subsLine[i];
+        }
+    }
+    void multiplyLineBy(MatrixElement* multipliedLine, MatrixElement Num, int size)
+    {
+        for (int i = 0; i < size; i++)
+        {
+            multipliedLine[i] *= Num;
+        }
+    }
+    void divideLineBy(MatrixElement* dividedLine, MatrixElement Num, int size)
+    {
+        if (Num == 0)
+            return;
+        for (int i = 0; i < size; i++)
+        {
+            dividedLine[i] = dividedLine[i] / Num;
+        }
+    }
+    void swapLines(MatrixElement* A1, MatrixElement* A2, int size)
+    {
+        double tmp;
+        for (int i = 0; i < size; i++)
+        {
+            tmp = A1[i];
+            A1[i] = A2[i];
+            A2[i] = tmp;
+        }
+    }
+    bool toDiagForm(Matrix& EM)
+    {
+        MatrixElement** E = EM.data;
+        MatrixElement** A = data;
+        int size = _v_size;
+
+        for (int i = 0; i < size; i++)
+        {
+            if (!A[i][i])
+            {
+                bool isFoundNoZero = false;
+                for (int m = i; m < size; m++)
+                {
+                    if (A[m][i])
+                    {
+                        isFoundNoZero = true;
+                        swapLines(A[m], A[i], size);
+                        swapLines(E[m], E[i], size);
+                        break;
+                    }
+                }
+                if (!isFoundNoZero)
+                    return false;
+            }
+
+            if (A[i][i] != 1)
+            {
+                divideLineBy(E[i], A[i][i], size);
+                divideLineBy(A[i], A[i][i], size);
+            }
+
+            double NOD;
+            for (int k = 0; k < size; k++)
+            {
+                if (k != i)
+                {
+                    NOD = A[k][i];
+                    if (NOD)
+                    {
+                        multiplyLineBy(A[i], NOD, size);
+                        multiplyLineBy(E[i], NOD, size);
+
+                        subLines(A[k], A[i], size);
+                        subLines(E[k], E[i], size);
+
+                        divideLineBy(A[i], NOD, size);
+                        divideLineBy(E[i], NOD, size);
+
+
+                        std::cout << "==================" << std::endl;
+                        (*this).console_output();
+                        std::cout << "------------------" << std::endl;
+                        EM.console_output();
+                        std::cout << "==================" << std::endl;
+                    }
+                }
+               
+            }
+        }
+    };
+    Matrix generateUnitMatrix()
+    {
+        int size = _h_size;
+        Matrix<MatrixElement> E(size, size);
+        for (int i = 0; i < size; i++)
+        {
+            for (int k = 0; k < size; k++)
+            {
+                if (i == k)
+                    E.data[i][i] = 1;
+                else
+                    E.data[i][k] = 0;
+            }
+        }
+        return E;
+    }
+   
 public:
+    Matrix()
+    {
+        _v_size = _h_size = 0;
+        data = nullptr;
+    }
     Matrix(const int vertical_size, const int horizontal_size)
     {
         _v_size = vertical_size;
@@ -51,6 +166,7 @@ public:
     {
         for (int i = 0; i < _v_size; i++)
             delete[] data[i];
+        delete data;
         _v_size = _h_size = 0;
     }; 
 
@@ -82,11 +198,16 @@ public:
         }
 
     }
-
     Matrix& operator= (const Matrix& original)
     {      
-        if (original._h_size != _h_size || original._v_size != _v_size)
-            throw std::runtime_error("Invalid matrix size\n");
+        for (int i = 0; i < _v_size; i++)
+        {
+            delete[] data[i];
+        }
+        delete[] data;
+
+        _v_size = original._v_size;
+        _h_size = original._h_size;
 
         for (int i = 0; i < _v_size; i++)
         {
@@ -111,6 +232,18 @@ public:
         }
 
         return C;
+    }
+   
+    Matrix operator!()
+    {
+        if (_h_size != _v_size)
+            throw std::runtime_error("Invalid matrix size\n");
+
+        Matrix<MatrixElement> C = (*this);
+        Matrix<MatrixElement> E = generateUnitMatrix();
+     
+        C.toDiagForm(E);
+        return E;
     }
 
     Matrix& operator+= (const Matrix& original)
@@ -180,11 +313,11 @@ public:
         return *this;
     }
 
-    MatrixElement* operator[](int h_idx)
+    MatrixElement* operator[](int v_idx)
     {
-        if (h_idx>_h_size)
+        if (v_idx > _v_size)
             throw std::runtime_error("Invalid matrix size\n");
-        return data[h_idx];
+        return data[v_idx];
     }
 
    
@@ -255,3 +388,4 @@ public:
    
     
 };
+
